acdce5e1cd8ca60acd3e8198fee3299ed442cdb2
Michael Jeries <michael.jeries@bluecrossmn.com>
2018-02-10 23:00:54 -0600

    initial commit

---
+++ src/SkunkApp.java
@@ -0,0 +1,13 @@
+
+
+public class SkunkApp {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        StdOut.print(SkunkApp.hello());
+    }
+    private static String hello() {
+    		return "Hello World";
+    }
+
+}
