b8172834ae914f77578611e7f13edbb431836c93
seis-602-fall2017 <darius.nyaundi@LAPTOP-QG5H3RPR>
2018-02-12 12:18:14 -0600

    dice rolls complete.

--- src/SkunkApp.java
+++ src/SkunkApp.java
@@ -1,13 +1,27 @@
-
+import java.util.Random;
 
 public class SkunkApp {
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        StdOut.print(SkunkApp.hello());
-    }
-    private static String hello() {
-    		return "Hello World";
-    }
+	public static void main(String[] args) {
 
+		int sum, dice1, dice2, actual = 0;
+		int[] Total = new int[13];
+		int[] ways = { 0, 0, 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1 };
+
+		for (int i = 0; i < 36000; i++) {
+			sum = 0;
+			dice1 = new Random().nextInt(6) + 1;
+			dice2 = new Random().nextInt(6) + 1;
+			sum = dice1 + dice2;
+			Total[sum]++;
+		}
+
+		for (int i = 0; i < 13; i++)
+			actual += Total[i];
+
+		System.out.printf("%-4s %s %-11s %s%n", "Sum", "Total", "Expected", "Actual");
+		for (int i = 2; i < 13; i++)
+			System.out.printf("%-4d %-5d %-10.5f %2.5f%n", i, Total[i], 100.0 * ways[i] / 36,
+					100 * (double) Total[i] / actual);
+	}
 }
