c22582f54c32f23b7876406f01f11551aa0f5984
seis-602-fall2017 <darius.nyaundi@10.50.70.10>
2018-02-17 10:17:14 -0600

    Fixed looping to fit project objective.

--- src/SkunkApp.java
+++ src/SkunkApp.java
@@ -1,20 +1,16 @@
 import java.util.Random;
-import java.util.Scanner;
 
 public class SkunkApp {
 
 	public static void main(String[] args) {
 
-		String again = "y"; // To control the loop
-
-		// Create a Scanner object to read keyboard input.
-		Scanner keyboard = new Scanner(System.in);
+		String again; // To control the loop
 
 		// Create a Random object to generate random numbers.
 		Random rand = new Random();
 
 		// rolling the dice.
-		while (again.equalsIgnoreCase("y")) {
+		{
 			System.out.println("Rolling");
 			int die1 = rand.nextInt(6) + 1;
 			int die2 = rand.nextInt(6) + 1;
@@ -22,7 +18,7 @@ public class SkunkApp {
 			System.out.println(die1);
 			System.out.println("dice 2");
 			System.out.println(die2);
-			again = keyboard.nextLine();
+
 		}
 	}
 }
