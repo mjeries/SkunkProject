fc9211de8ce3900cb4390f57f2d21237a2fa59a7
Michael Jeries <michael.jeries@bluecrossmn.com>
2018-02-18 22:21:30 -0600

    probably should refactor rollidie method into a different class

--- src/com/seis635/SkunkApp.java
+++ src/com/seis635/SkunkApp.java
@@ -3,22 +3,18 @@ package com.seis635;
 import java.util.Random;
 
 public class SkunkApp {
-    private static Random rand = new Random();
+    public SkunkApp(){
+        this.rand  = new Random();
+    }
+    private Random rand;
 	public static void main(String[] args) {
 
-		String again; // To control the loop
+        System.out.println("Rolling");
+        System.out.println("dice 1");
+        System.out.println(new SkunkApp().rollDie());
+        System.out.println("dice 2");
+        System.out.println(new SkunkApp().rollDie());
 
-		// rolling the dice.
-		{
-			System.out.println("Rolling");
-			int die1 = rand.nextInt(6) + 1;
-			int die2 = rand.nextInt(6) + 1;
-			System.out.println("dice 1");
-			System.out.println(die1);
-			System.out.println("dice 2");
-			System.out.println(die2);
-
-		}
 	}
 	public int rollDie() {
 	    int die = this.rand.nextInt(6) + 1;
