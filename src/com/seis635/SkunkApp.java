0c36f6be661e24d86304518f0d5f52950e939d20
seis-602-fall2017 <darius.nyaundi@LAPTOP-QG5H3RPR>
2018-02-28 10:51:24 -0600

    Ceating Player 1 and Player 2.

--- src/com/seis635/SkunkApp.java
+++ src/com/seis635/SkunkApp.java
@@ -9,17 +9,26 @@ public class SkunkApp {
     private Random rand;
 	public static void main(String[] args) {
 
-        System.out.println("Rolling");
+        System.out.println("player1");
         System.out.println("dice 1");
         System.out.println(new SkunkApp().rollDie());
         System.out.println("dice 2");
         System.out.println(new SkunkApp().rollDie());
 
-	}
-	public int rollDie() {
-	    int die = this.rand.nextInt(6) + 1;
-        return die;
-    }
+        {
+
+
+    System.out.println("player2");
+    System.out.println("dice 1");
+    System.out.println(new SkunkApp().rollDie());
+    System.out.println("dice 2");
+    System.out.println(new SkunkApp().rollDie());
+
+}}
+public int rollDie() {
+    int die = this.rand.nextInt(6) + 1;
+    return die;
+}
 
 
 }
