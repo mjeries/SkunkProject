314d041f636e17894921050a0fd0311c83e60333
seis-602-fall2017 <darius.nyaundi@LAPTOP-QG5H3RPR>
2018-03-01 11:22:07 -0600

    Counting each roll. Dice one and Dice 2.

--- src/com/seis635/SkunkApp.java
+++ src/com/seis635/SkunkApp.java
@@ -3,32 +3,27 @@ package com.seis635;
 import java.util.Random;
 
 public class SkunkApp {
-    public SkunkApp(){
-        this.rand  = new Random();
-    }
-    private Random rand;
+
 	public static void main(String[] args) {
+		 
+Random die1 = new Random();
+Random die2 = new Random();
+int counter;
+int hrolls = 10;
+int sides = 6;
 
-        System.out.println("PlayerOne");
-        System.out.println("dice 1");
-        System.out.println(new SkunkApp().rollDie());
-        System.out.println("dice 2");
-        System.out.println(new SkunkApp().rollDie());
+for(counter=0; counter < hrolls; ++counter) {
+	int rollnumber = counter +1;
+	int x = die1.nextInt(sides);
+++x;
+System.out.println("Round " + rollnumber + " die 1 rolled a " +x);
+int y = die2.nextInt(sides);
+++ y;
 
-        {
+System.out.println("Round " + rollnumber + " die 2 rolled a " +y);
 
+int rollTotal = x+ y;
 
-    System.out.println("PlayerTwo");
-    System.out.println("dice 1");
-    System.out.println(new SkunkApp().rollDie());
-    System.out.println("dice 2");
-    System.out.println(new SkunkApp().rollDie());
-
-}}
-public int rollDie() {
-    int die = this.rand.nextInt(6) + 1;
-    return die;
-}
-
-
-}
+System.out.println("the total "   + rollTotal);
+System.out.println("");
+}}}
