aa4b9797e978d226fec5289d00d203ee0d0a6a26
darius-nyaundi <darius.nyaundi@LAPTOP-QG5H3RPR>
2018-03-03 20:02:26 -0600

    JUnit testing for player2

---
+++ SkunkAppTest/com/seis635/P2.java
@@ -0,0 +1,36 @@
+package com.seis635;
+
+import com.seis635.SkunkApp;
+
+import java.util.Random;
+
+import org.junit.*;
+
+public class P2{
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	
+		public void testingplayer1total() {
+			int q = 1;
+			int p = 1;
+			int rollTotal = q+ p;
+			int overalltotal = rollTotal;
+		overalltotal = 2;
+	}
+	}
