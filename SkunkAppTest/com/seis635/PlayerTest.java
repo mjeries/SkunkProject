1ee5e0ff384200c0178434eceb625cb8a4ad6315
Darius nyaundi <darius.nyaundi@LAPTOP-QG5H3RPR>
2018-03-15 11:18:12 -0500

    Starting new test classes for correct testing.

---
+++ SkunkAppTest/com/seis635/PlayerTest.java
@@ -0,0 +1,49 @@
+package com.seis635;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class PlayerTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testPlayer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTotalScore() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTotalScore() {
+		fail("Not yet implemented");
+	}
+
+}
